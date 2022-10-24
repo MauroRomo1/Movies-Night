7d1d93f9dcd7a6ff35f89dc4b3ff0477501e85e6
Mauro Romo <mauroromo1999outlook.com>
2022-10-24 11:27:13 -0300

    Cambiando la estructura del navbar de admin para que sea igual a la del resto de paginas

--- js/admin.js
+++ js/admin.js
@@ -8,43 +8,106 @@ let contenidoTabla = document.querySelector("#cuerpoTabla");
 
 let form = document.querySelector("#formulario");
 
+//capturamos el modal que usamos para mostrar el usuario logueado.
+let myModalUser = new bootstrap.Modal(document.getElementById("usuarioModal"), {
+  keyboard: false,
+});
+
+// Capturamos el modal para modificar a los usuarios de la tabla
 const myModal = new bootstrap.Modal("#updateModal", {
   keyboard: false,
 });
 
-// info admin logueado
-let adminContenedor = document.querySelector("#contendorAdminCard");
-
-let adminCard = `
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img
-                    src="../images/avatares/${usuario.avatar}.jpg"
-                    class="img-fluid"
-                    alt="${usuario.username}"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">${usuario.username}</h5>
-                    <p class="card-text">
-                      ${usuario.email}
-                    </p>
-                    <p class="card-text">
-                      <small class="text-muted">${usuario.rol}</small>
-                    </p>
-                    <button id="volverInicio" class="btn btn-primary">Volver al inicio</button>
-                    <button id="logout" class="btn btn-secondary">Cerrar sesión</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-  `;
-adminContenedor.innerHTML = adminCard;
-
 let indiceUser = null;
 
+// Verificamos si el usuario esta logueado
+const usuarioRegistrado = function name() {
+  if (!usuario) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// Modificamos el navbar segun si el usuario esta logueado o no.
+const modificarNavbar = function () {
+  if (usuarioRegistrado()) {
+    let contenedorBotones = document.querySelector("#contenedor_botones");
+
+    let estructuraNavbar = `
+  <div class="dropdown">
+        <a
+          class="dropdown-toggle text-white text-decoration-none"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <img
+            src="../images/avatares/${usuario.avatar}.jpg"
+            alt="avatar"
+            id="avatar"
+          />
+        </a>
+
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+          <li><a class="dropdown-item" href="#" id="verUsuario">Perfil</a></li>
+          ${
+            usuario.rol === "admin_rol"
+              ? '<li><a class="dropdown-item" href="#">Administracion</a></li>'
+              : '<li><a class="dropdown-item" href="#">My lista</a></li>'
+          }
+        </ul>
+  </div>
+  `;
+    contenedorBotones.innerHTML = estructuraNavbar;
+  }
+};
+modificarNavbar();
+
+if (!usuario || usuario.rol === "user_rol") {
+  let contenedorMain = document.querySelector("#contenedor");
+
+  let mensaje = `
+<h3 class="text-center">No tienes los permisos necesarios para estar aquí</h3>
+`;
+  contenedorMain.innerHTML = mensaje;
+}
+
+const CargarInfoModalUser = function () {
+  if (usuarioRegistrado()) {
+    let contenedorModal = document.querySelector("#card_avatar");
+
+    let estructuraAvatar = `
+            <div class="card border-0 text-center" style="max-width: 540px">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img
+                      src="../images/avatares/${usuario.avatar}.jpg"
+                      class="img-fluid"
+                      id="avatarModal"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${usuario.username}</h5>
+                      <p class="card-text">
+                        ${usuario.email}
+                      </p>
+                      <p class="card-text">
+                      <b>Rol:</b>
+                        ${usuario.rol}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+             </div>
+  `;
+    contenedorModal.innerHTML = estructuraAvatar;
+  }
+};
+CargarInfoModalUser();
 // Abrir modal
 const abrirModal = function (indice) {
   myModal.show();
@@ -148,11 +211,13 @@ const updateUsuario = function (e) {
 
 cargarTabla();
 
+//Deslogueo de la pagina
 document.querySelector("#logout").addEventListener("click", function () {
   localStorage.removeItem("usuario");
   location.href = "../pages/login.html";
 });
 
-document.querySelector("#volverInicio").addEventListener("click", function () {
-  location.href = "../index.html";
+//Si hacemos click en el boton para ver perfil
+document.querySelector("#verUsuario").addEventListener("click", function () {
+  myModalUser.show();
 });

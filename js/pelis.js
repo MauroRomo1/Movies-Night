5711f9d4d0ba46e95598b03b3007cbb83581cc17
Mauro Romo <mauroromo1999outlook.com>
2022-11-05 16:28:37 -0300

    Eliminado imagen de login que no se utiliza, y hardcodeado peliculas para visualizar

---
+++ js/pelis.js
@@ -0,0 +1,152 @@
+let peliculasHardcodeadas = [
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/21/10/18/14/15/2785937.jpg",
+    banner:
+      "https://nwsmovdaily.com/wp-content/uploads/2022/07/the-black-phone-poster-scott-derrickson-social.jpg",
+    id: 1667263185697,
+    nombre: "The Black Phone",
+    descripcion:
+      "Un homicida sádico y enmascarado mantiene a Finney, un niño de 13 años, secuestrado en un sótano incomunicado. A través de un teléfono averiado que hay en la pared, Finney se comunica con otras víctimas del criminal, quienes quieren ayudarlo.",
+    categoria: "Terror",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada: "https://pics.filmaffinity.com/Samaritan-804076396-large.jpg",
+    banner:
+      "https://wegotthiscovered.com/wp-content/uploads/2022/07/samaritan_banner.jpg",
+    id: 1667263274633,
+    nombre: "Samaritan",
+    descripcion:
+      "Un joven descubre que el superhéroe al que todos admiraban, y al que creían muerto, podría seguir vivo.",
+    categoria: "Accion",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/09/28/08/51/0848185.jpg",
+    banner:
+      "https://www.heavenofhorror.com/wp-content/uploads/2022/10/mr-harrigans-phone-2022-netflix-1200x720.jpg",
+    id: 1667510900556,
+    nombre: "El teléfono del señor Harrigan",
+    descripcion:
+      "El teléfono del señor Harrigan es una película de intriga estadounidense dirigida y escrita por John Lee Hancock, basada en el cuento del mismo nombre de Stephen King, publicada en su colección de cuentos La sangre manda.",
+    categoria: "Terror",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://pics.filmaffinity.com/La_matanza_de_Texas-642644129-large.jpg",
+    banner:
+      "https://ntvb.tmsimg.com/assets/p21339058_v_h10_aa.jpg?w=1280&h=720",
+    id: 1667512599359,
+    nombre: "texas chainsaw massacre (2022)",
+    descripcion:
+      "Un grupo de influencers quiere darle una nueva vida a un pueblo abandonado de Texas, pero se encuentran con el legendario asesino en serie Leatherface.",
+    categoria: "Terror",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/21/12/13/17/32/3629040.jpg",
+    banner: "https://i.ytimg.com/vi/WUsF05mugR4/maxresdefault.jpg",
+    id: 1667516391448,
+    nombre: "Scream 5",
+    descripcion:
+      "Veinticinco años después de la masacre ocurrida en Woodsboro, un nuevo asesino se pone la legendaria máscara para matar adolescentes.",
+    categoria: "Terror",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img2.acsta.net/pictures/22/04/25/10/39/2475018.jpg",
+    banner: "https://i.ytimg.com/vi/p2e7zsZhzxo/maxresdefault.jpg",
+    id: 1667577320850,
+    nombre: "Jurassic World: Dominion",
+    descripcion:
+      "La isla Nublar y su parque han sido destruidos, pero el problema no se terminó. Con los dinosaurios dispersos por todo el mundo, la convivencia entre el presente y el pasado alcanza un nuevo nivel de tensión.",
+    categoria: "Accion",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/01/13/15/49/5165456.jpg",
+    banner: "https://i.ytimg.com/vi/62bKO3LP6HA/maxresdefault.jpg",
+    id: 1667577836539,
+    nombre: "Uncharted: Fuera del mapa",
+    descripcion:
+      "El cazador de tesoros Victor Sullivan recluta a Nathan Drake para que lo ayude a recuperar una fortuna de 500 años de antigüedad. Lo que comienza como un atraco se convierte en una competencia contra el despiadado Santiago Moncada.",
+    categoria: "Accion",
+    publicada: true,
+    destacado: true,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/02/10/13/16/1386612.jpg",
+    banner: "https://i.ytimg.com/vi/9eQBl3_6FKA/maxresdefault.jpg",
+    id: 1667578030748,
+    nombre: "Morbius ",
+    descripcion:
+      "Al intentar encontrar una cura para una extraña enfermedad en su sangre, un doctor se transforma en un vampiro.",
+    categoria: "Accion",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/05/05/17/26/5021573.jpg",
+    banner: "https://i.ytimg.com/vi/_eWt8mV6dBM/maxresdefault.jpg",
+    id: 1667588334023,
+    nombre: "Lightyear",
+    descripcion:
+      "Buzz Lightyear se embarca en una aventura intergaláctica con un grupo de reclutas ambiciosos y su compañero robot. Los amigos tendrán que aprender a trabajar juntos para escapar del malvado Zurg y su ejército de robots.",
+    categoria: "Animada",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/02/18/10/20/5195258.jpg",
+    banner:
+      "https://tierragamer.com/wp-content/uploads/2022/05/Sonic-2-Pelicula.jpg",
+    id: 1667592504953,
+    nombre: "Sonic 2: La película",
+    descripcion:
+      "Sonic y su compañero Tails emprenden un viaje alrededor del mundo en busca de una esmeralda que tiene del poder de destruir civilizaciones.",
+    categoria: "Animada",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/05/25/12/28/4179295.jpg",
+    banner: "https://i.ytimg.com/vi/W27moupirnI/maxresdefault.jpg",
+    id: 1667594465721,
+    nombre: "Minions: nace un villano",
+    descripcion:
+      'En los años 70, Gru crece siendo un gran admirador de "Los salvajes seis", un supergrupo de villanos. Para demostrarles que puede ser malvado, diseña un plan con la esperanza de formar parte de la banda.',
+    categoria: "Animada",
+    publicada: true,
+    destacado: false,
+  },
+  {
+    portada:
+      "https://es.web.img3.acsta.net/pictures/22/08/30/13/02/2562155.jpg",
+    banner: "https://i.ytimg.com/vi/yuOGgqs1Nl4/maxresdefault.jpg",
+    id: 1667594638989,
+    nombre: "Pinocho (2022)",
+    descripcion:
+      "En un pueblo italiano, el títere de madera Pinocho cobra vida gracias al Hada Azul. Pinocho se esfuerza por comportarse como un niño de carne y hueso, pero su vida da un vuelco al abandonar a su padre para unirse a un circo.",
+    categoria: "Animada",
+    publicada: true,
+    destacado: false,
+  },
+];
+
+localStorage.setItem("peliculas", JSON.stringify(peliculasHardcodeadas));
